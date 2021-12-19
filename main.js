d6704f8928daa26007a813b0178cdb6225be67b3
quayum quayum <96136071+abdulquayumhp@users.noreply.github.com>
2021-12-20 00:47:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+$('.font-btn').click(function () {
+        $('.text-hide').slideDown();
+        $('.font-btn').hide()
+        $('.hide-btn').show()
+});
+$('.hide-btn').click(function () {
+        $('.text-hide').slideUp();
+        $('.hide-btn').hide()
+        $('.font-btn').show()
+});
+// 2
+$('.font-btn-two').click(function () {
+        $('.text-hide-two').slideDown();
+        $('.font-btn-two').hide()
+        $('.hide-btn-two').show()
+});
+$('.hide-btn-two').click(function () {
+        $('.text-hide-two').slideUp();
+        $('.hide-btn-two').hide()
+        $('.font-btn-two').show()
+});
+
+
+
+// 3
+$('.font-btn-three').click(function () {
+        $('.text-hide-three').slideDown();
+        $('.font-btn-three').hide()
+        $('.hide-btn-three').show()
+});
+$('.hide-btn-three').click(function () {
+        $('.text-hide-three').slideUp();
+        $('.hide-btn-three').hide()
+        $('.font-btn-three').show()
+});
+
+// 4
+
+$('.font-btn-two-one').click(function () {
+        $('.text-hide-four').slideDown();
+        $('.font-btn-two-one').hide()
+        $('.hide-btn-two-one').show()
+});
+$('.hide-btn-two-one').click(function () {
+        $('.text-hide-four').slideUp();
+        $('.hide-btn-two-one').hide()
+        $('.font-btn-two-one').show()
+});
+
+// 5
+
+$('.font-btn-two-two').click(function () {
+        $('.text-hide-five').slideDown();
+        $('.font-btn-two-two').hide()
+        $('.hide-btn-two-two').show()
+});
+$('.hide-btn-two-two').click(function () {
+        $('.text-hide-five').slideUp();
+        $('.hide-btn-two-two').hide()
+        $('.font-btn-two-two').show()
+});
+
+
+// 6
+
+$('.font-btn-two-three').click(function () {
+        $('.text-hide-six').slideDown();
+        $('.font-btn-two-three').hide()
+        $('.hide-btn-two-three').show()
+});
+$('.hide-btn-two-three').click(function () {
+        $('.text-hide-six').slideUp();
+        $('.hide-btn-two-three').hide()
+        $('.font-btn-two-three').show()
+});
+
+
+// menu bar
+
+$('.show').click(function () {
+        $('.menubar-two').slideDown();
+        $('.show').hide();
+        $('.hide').show();
+});
+$('.hide').click(function () {
+        $('.menubar-two').slideUp();
+        $('.hide').hide();
+        $('.show').show();
+});
